13bd53eb51743df08a56298becba5f69099cab1e
Eddie Lichtman <e.lichtman2@gmail.com>
2023-02-10 11:51:06 -0600

    Adds NullableStringInIdentity.cs example

---
+++ DefectExamples/NullableStringInIdentity.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Principal;
+
+namespace TestMinimalApiSwagger.DefectExamples
+{
+    public class NullableStringInIdentity
+    {
+        public string TestThisShouldBeNonNullable(IIdentity identity)
+        {
+            return identity.Name;
+        }
+    }
+}
